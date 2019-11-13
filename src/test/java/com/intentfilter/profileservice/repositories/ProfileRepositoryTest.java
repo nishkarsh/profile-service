849a71ae04c6417722986e23c6870b2ba9d5d7ca
nishkarsh <nishkarsh4@gmail.com>
2019-11-13 10:50:38 +0530

    Create test profile, using for integration tests.

--- src/test/java/com/intentfilter/profileservice/repositories/ProfileRepositoryTest.java
+++ src/test/java/com/intentfilter/profileservice/repositories/ProfileRepositoryTest.java
@@ -3,11 +3,15 @@ package com.intentfilter.profileservice.repositories;
 import com.intentfilter.profileservice.models.Profile;
 import io.github.glytching.junit.extension.random.Random;
 import io.github.glytching.junit.extension.random.RandomBeansExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -16,10 +20,20 @@ import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith({MockitoExtension.class, RandomBeansExtension.class})
 class ProfileRepositoryTest {
     @Autowired
     private ProfileRepository repository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private static final String COLLECTION = "ps.profiles";
+
+    @BeforeEach
+    void setUp() {
+        mongoTemplate.dropCollection(COLLECTION);
+    }
 
     @Test
     void shouldInsertProfile(@Random Profile profile) {
@@ -43,5 +57,10 @@ class ProfileRepositoryTest {
 
         assertTrue(foundProfile.isPresent());
         assertThat(foundProfile.get(), is(updatedProfile));
+    }
+
+    @AfterEach
+    void tearDown() {
+        mongoTemplate.dropCollection(COLLECTION);
     }
 }
